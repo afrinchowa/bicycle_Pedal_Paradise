10477acb72a4cb9b3509daf9dbeea25e60c0ae09
chowa <arfinchowa524@gmail.com>
2025-04-08 01:56:20 +0600

    some query solved over the update and delete operation

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,8 +1,11 @@
 export type TUser = {
-    id: string;
-    password: string;
-    needsPasswordChange: boolean;
-    role: 'admin' | 'student' | 'faculty';
-    status: 'in-progress' | 'blocked';
-    isDeleted: boolean;
-  };
+  id: string; // <-- Add this
+  name: string;
+  email: string;
+  photo?: string | null;
+  password: string;
+  role: 'admin' | 'user';
+  userStatus?: 'in-progress' | 'blocked';
+  createdAt?: Date;
+  updatedAt?: Date;
+};
